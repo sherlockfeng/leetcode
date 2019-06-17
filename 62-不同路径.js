457c03c351a29bc03c7f0030e476b8e50b0d29eb
heyunfeng <heyunfeng@baidu.com>
2019-06-17 17:39:38 +0800

    优化算法

--- 62-不同路径.js
+++ 62-不同路径.js
@@ -4,22 +4,18 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    let temp = [];
-    for (let i = 0; i < n; i++) {
-      temp[i] = Array(m).fill(0)
+    if (m <=0 || n<=0) {
+        return 1;
     }
-    console.log(temp);
+    let temp = Array(m).fill(0);
+    temp[0] = 1;
+    console.log(temp)
     for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
-            if (i === 0 || j === 0) {
-                temp[i][j] = 1;
-            }
-            else if (!temp[i][j]) {
-                temp[i][j] = temp[i - 1][j] + temp[i][j - 1];
-            }
+        for (let j = 1; j < m; j++) {
+            temp[j] += temp[j - 1];
         }
     }
-    return temp[n - 1][m - 1];
+    return temp[m- 1];
 };
 
 console.log(uniquePaths(3, 2))
