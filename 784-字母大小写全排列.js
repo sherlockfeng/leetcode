7947ea005d5f5aa8c976703b2aa369336e8342c8
heyunfeng <heyunfeng@baidu.com>
2019-06-19 16:03:18 +0800

    完成784

---
+++ 784-字母大小写全排列.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} S
+ * @return {string[]}
+ */
+var letterCasePermutation = function(S) {
+    let result = [];
+    const Rex = /[a-zA-Z]/;
+    const dfs = (str, index) => {
+        if (index === S.length) {
+            result.push(str);
+            return;
+        }
+        if (Rex.test(S[index])) {
+            let temp = str.substring(0, index) + str[index].toUpperCase() + str.substring(index + 1);
+            let temp1 = str.substring(0, index) + str[index].toLowerCase() + str.substring(index + 1);
+            dfs(temp1, index + 1);
+            dfs(temp, index+ 1);
+        }
+        else {
+            dfs(str, index + 1);
+        }
+    }
+    dfs(S, 0);
+    return result;
+};
+
+console.log(letterCasePermutation('C'))
